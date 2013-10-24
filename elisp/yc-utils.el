cd8e6b2e78e1515c7294e1e396464ea63df76c7d
yangyingchao <yangyingchao@gmail.com>
2013-10-24 17:51:43 +0800

    added a binary search function.

--- elisp/yc-utils.el
+++ elisp/yc-utils.el
@@ -1,12 +1,5 @@
 ;;; -*- emacs-lisp -*- -*- coding: utf-8; -*-
 ;;;
-;;; File: yc-utils.el
-;;; Author: Alex Ott <alexott@gmail.com>
-;;;
-;;; Created: 10-26-2012
-;;;
-;;;
-;;;
 
 (defconst r-match-cost
   (rx line-start (+? ascii) "TileKeys:" (* blank)
@@ -56,6 +49,42 @@
   (print (yc/mean "b.txt"))
   nil
 )
+
+(defun binary-search (x lst &optional l h)
+  (if (or (not (listp lst))
+          (listp x))
+      (message "Wrong type of arguments, usage: (binary-search  ele list)")
+    (let* ((l (if l l 0))
+           (h (if h h (length lst)))
+           (n (/  (+ h l) 2))
+           (v (nth n lst)))
+      (cond
+       ((or (not v) (= v x) (>= l h)) n)
+       ((< x v) (binary-search x lst l (1- n)))
+       ((> x v) (binary-search x lst (1+ n) h))
+       )
+      )
+    )
+  )
+
+
+;; (binary-search 7341 (let ((i 0)
+;;       (a nil))
+;;   (while (< i 100000)
+;;     (setq a (cons i a)
+;;           i (1+ i))
+;;       )
+;;   (nreverse a)))
+
+
+
+
+
+
+
+
+
+
 
 (provide 'yc-utils)
 ;;;;; yc-utils.el ends here
