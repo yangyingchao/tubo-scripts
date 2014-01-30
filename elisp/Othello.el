3d1830866d25a3f7b3f82e7d55d5a77d4d97158a
yangyingchao <yangyingchao@gmail.com>
2014-01-30 14:08:28 +0800

    two players: one use o-strategy-simple, one use max-disc

--- elisp/Othello.el
+++ elisp/Othello.el
@@ -15,11 +15,8 @@
         (cons 1 1) (cons 1 -1) (cons -1 -1) (cons -1 1))
   "Descriptions.")
 
-(defun o-str-pos (p)
-  "description"
-  (if p
-      (format "%d:%d" (car p) (cdr p))
-    "nil"))
+(defun o-str-pos (p) "description"
+  (if p (format "%d:%d" (car p) (cdr p)) "nil"))
 
 
 (defun o-pos+ (p1 p2)
@@ -70,9 +67,11 @@
   (setq o-index 1
         o-logs ""))
 
+(defvar o-debug nil "nil")
+
 (defun o-board-draw ()
   "Draw play board"
-  (let ((buffer (get-buffer-create "*Othello*"))
+  (let ((buffer (get-buffer-create (if o-debug (format "*Othello-%d*" o-index) "*Othello*")))
         c)
     (with-current-buffer buffer
       (erase-buffer)
@@ -90,35 +89,41 @@
       (insert (format "\nSteps:\n%s" o-logs))
       (goto-char (point-min)))))
 
-(defvar o-current-strategy 'o-strategy-max-disc
-  "Current strategy used by this resolver.")
+;;; Strategies begins here ...
 
-(defun o-strategy-max-disc (c pos table)
-  "Maximum Disc strategy."
+(defun o-evaluate-postion (c pos table)
+  "Evaluate specified position, find out how many discs it will earn."
   (let ((tx 0)
-        (cc (gethash pos o-table))
+        (cc (gethash pos table))
         step)
     (when (not cc)
       (dolist  (step o-steps)
-        (let* ((n-pos (o-pos+ pos step))
-               (nc (gethash n-pos o-table)))
+        (let* ((t-pos (o-pos+ pos step))
+               (nc (gethash t-pos table))
+               n-pos)
           (while (and nc (not (equal nc c)))
-            (setq n-pos (o-pos+ n-pos step)
-                  nc (gethash n-pos o-table)))
-          (when (equal nc c)
+            (setq n-pos t-pos
+                  t-pos (o-pos+ t-pos step)
+                  nc (gethash n-pos table)))
+          (when (and n-pos (equal (gethash n-pos table) c))
             (setq tx (+ tx (o-distance n-pos pos)))))))
+    ;; (message "Returning %d for %s -- %s" tx (o-str-pos pos) (symbol-name c))
     tx))
 
-(defun o-evaluate-postion (c pos table &optional depth)
-  "Evaluate given position
-Depth not supported for now."
-  (let ((func (if o-current-strategy o-current-strategy 'o-strategy-max-disc)))
-    (funcall o-current-strategy c pos table)))
+(defun o-strategy-simple (c table depth)
+  "Most simple strategy
+Just find the first possible persition."
+  (catch 'TAG
+    (o-loop-for-size
+     nil
+     (let* ((n-pos (cons i j))
+            (tx (o-evaluate-postion c n-pos table)))
+       (if (> tx 0)
+           (throw 'TAG n-pos))))
+    TAG))
 
-(defun o-get-best-step (c &optional depth)
-  "get next best stop for color c.
-It uses Maximum Disc Strategy which is very bad...
-depth is not supported for now."
+(defun o-strategy-max-disc (c table depth)
+  "Maximum Disc strategy."
   (let ((mx 0)
         (tx 0)
         c-pos
@@ -126,13 +131,22 @@ depth is not supported for now."
     (o-loop-for-size
      nil
      (setq c-pos (cons i j))
-     (when (> (setq tx (o-evaluate-postion c c-pos (copy-hash-table o-table) 0)) mx)
+     (when (> (setq tx (o-evaluate-postion c c-pos (copy-hash-table o-table))) mx)
        (message "Updating max: (%s)%d -- (%s)%d"
                 (o-str-pos c-pos) tx
                 (o-str-pos f-pos)  mx)
        (setq mx tx
              f-pos c-pos)))
     f-pos))
+
+;;;  Strategies ends here.
+
+(defun o-get-best-step (c &optional o-strategy &optional depth)
+  "get next best stop for color c.
+It uses Maximum Disc Strategy which is very bad...
+depth is not supported for now."
+  (let ((strategy (if o-strategy o-strategy 'o-strategy-simple)))
+    (funcall strategy c (copy-hash-table o-table) 0)))
 
 
 (defun o-update-board (pos c)
@@ -170,14 +184,16 @@ depth is not supported for now."
      (cl-case (gethash (cons i j) o-table)
        ('b (setq bs (1+ bs)))
        ('w (setq ws (1+ ws)))))
-
     (cons ws bs)))
 
 (defun o-thello-self-play (c)
   "Function called by timer to play with itself"
   (let (pos stop)
     (if (not c) (setq stop t)
-      (setq pos (o-get-best-step c))
+      (setq pos (o-get-best-step c
+                                 (if (equal c 'b)
+                                     'o-strategy-max-disc ;; Black uses max-disc
+                                   'o-strategy-simple)))  ;; white uses simple.
       (setq o-logs
             (concat (format "\n%02d --- Color: %s, BestPos: %s"
                             o-index (symbol-name c) (o-str-pos pos))
@@ -190,7 +206,7 @@ depth is not supported for now."
         (o-board-draw)
         (setq c (if (eq c 'b) 'w 'b))))
     (if (not stop)
-        (setq o-global-timer (run-at-time "0.01 sec" nil 'o-thello-self-play c))
+        (setq o-global-timer (run-at-time "1 sec" nil 'o-thello-self-play c))
 
       ;; Last step, cancel timer and calculate statistics.
       (if o-global-timer
@@ -210,6 +226,8 @@ depth is not supported for now."
   (if (and (integerp sz)
            (> sz 2))
       (setq o-size sz))
+  (if o-global-timer
+      (cancel-timer o-global-timer))
   (with-current-buffer (get-buffer-create "*Othello*")
     (set-window-buffer (get-buffer-window)  (current-buffer)))
   (o-board-init)
